1570f1baa1f423738c09ab11f27b0a76f066b662
Paulo <ph.fernands90@gmail.com>
2022-08-11 00:35:16 -0300

    Implementando Interceptadores na aplicação

---
+++ mudi/src/main/java/br/com/alura/mvc/mudi/api/AcessosRest.java
@@ -0,0 +1,21 @@
+package br.com.alura.mvc.mudi.api;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.mvc.mudi.interceptor.InterceptadordeAcessos;
+import br.com.alura.mvc.mudi.interceptor.InterceptadordeAcessos.Acesso;
+
+@RequestMapping("acessos")
+@RestController
+public class AcessosRest {
+	
+	@GetMapping
+	public List<Acesso> getAcessos() {
+		return InterceptadordeAcessos.acessos;
+	}
+
+}
